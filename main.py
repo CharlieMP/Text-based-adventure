7fb756f065cf569dccb53fb0ea87d3f05f80e43d
CharlieMP <pudseyc@macstudent.com.au>
2025-05-22 02:59:58 +0000

    added a simple gameplay loop

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ cavern.link_cave(dungeon, "south")
 grotto.link_cave(dungeon, "east")
 dungeon.link_cave(grotto, "west")
 dungeon.link_cave(cavern, "north")
-dungeon.get_details()
+current_cave = cavern
+while True:
+    print("\n")
+    current_cave.get_details()
+    command = input("> ")
+    current_cave = current_cave.move(command)
